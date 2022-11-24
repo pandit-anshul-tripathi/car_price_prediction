8ed1e2136b90368fd004c60c7f6a305cd9255c6d
pandit-anshul-tripathi <83879503+pandit-anshul-tripathi@users.noreply.github.com>
2022-11-24 19:06:01 +0530

    Update data.py

---
+++ data.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+def app(cars_df):
+	st.header('View Data')
+	with st.beta_expander('View Dataset'):
+		st.table(cars_df)
+	st.subheader('Column Description:')
+	if st.checkbox('Show Summary'):
+		st.table(cars_df.describe())
+	beta_col1, beta_col2, beta_col3 = st.beta_columns(3)
+	with beta_col1:
+		if st.checkbox('Show all columns names'):
+			st.table(cars_df.columns)
+	with beta_col2:
+		if st.checkbox('View column data'):
+			column_data = st.selectbox('Select column', tuple('enginesize', 'horsepower', 'carwidth', 'drivewheel', 'price'))
+			if column_data == 'enginesize':
+				st.write(cars_df['enginesize'])
+			elif column_data == 'horsepower':
+				st.write(cars_df['horsepower'])
+			elif column_data == 'carwidth':
+				st.write(cars_df['carwidth'])
+			elif column_data == 'drivewheel':
+				st.write(cars_df['drivewheel'])
+			else:
+				st.write(cars_df['price'])
+	with beta_col3:
+        if st.checkbox("View column data-type"):
+            st.table(car_df.dtypes)
